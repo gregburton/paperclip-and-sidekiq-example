d1ccbf7186c6167cc699de54e0edb13f05b8131f
gregurton <gburt802@gmail.com>
2014-11-12 23:53:14 -0500

    made an attempt at start-end time code for cropping audio

--- app/workers/ringtone_crop_worker.rb
+++ app/workers/ringtone_crop_worker.rb
@@ -7,7 +7,8 @@ class RingtoneCropWorker
     ringtone = Ringtone.find(id)
     source = ringtone.source
     outfile = Paperclip::Tempfile.new(['ringtone', '.mp3'])
-    Paperclip.run("ffmpeg", "-i #{source.path} -y -t 30 -acodec copy #{outfile.path}" )
+    Paperclip.run("ffmpeg", "-i #{source.path} -ss #{source.start_time} -y -to #{source.end_time} -t 30 -acodec copy #{outfile.path}" )
+    #CANONICAL VERSION: Paperclip.run("ffmpeg", "-i #{source.path} -y -t 30 -acodec copy #{outfile.path}" )
     ringtone.ringtone = outfile
     ringtone.processing = false
     ringtone.save
